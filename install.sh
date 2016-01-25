732f258b54cba45f4de528e4c6636b621a7478cb
pawel <pawel.lewtak@xstream.net>
2016-01-25 12:55:49 +0100

    Initial commit with zshrc and intelij IDE custom options

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Zsh
+ln -f ~/.dotfiles/zshrc ~/.zshrc
+
+# PHPStorm/PyCharm
+ln -f ~/.dotfiles/intelij/intelij.options ~/.WebIde110/phpstorm64.vmoptions
+ln -f ~/.dotfiles/intelij/intelij.options ~/.PyCharm50/pycharm64.vmoptions
