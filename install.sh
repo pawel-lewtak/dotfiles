23f8070dc9206042d700b3e82bcffcdeac21bd66
pawel <pawel.lewtak@xstream.net>
2016-12-05 15:56:40 +0100

    Update install script for JetBrains IDE

--- install.sh
+++ install.sh
@@ -12,13 +12,13 @@ ln -f ~/.dotfiles/wgetrc ~/.wgetrc
 ln -f ~/.dotfiles/zshrc ~/.zshrc
 
 # Jetbrains IDE
-if [ -d ~/.PhpStorm2016.1 ]; then
+if [ -d ~/.PhpStorm2016.3 ]; then
     ln -f ~/.dotfiles/intelij/intelij.options ~/.PhpStorm2016.1/phpstorm64.vmoptions
 fi
-if [ -d ~/.PyCharm2016.1 ]; then
+if [ -d ~/.PyCharm2016.3 ]; then
     ln -f ~/.dotfiles/intelij/intelij.options ~/.PyCharm2016.1/pycharm64.vmoptions
 fi
-if [ -d ~/.IntelliJIdea2016.1 ]; then
+if [ -d ~/.IntelliJIdea2016.3 ]; then
     ln -f ~/.dotfiles/intelij/intelij.options ~/.IntelliJIdea2016.1/idea64.vmoptions
 fi
 
